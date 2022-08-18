bea9ff7341473d3503a3c5ae19fde15f906b5c82
ticdevclub <106364479+ticdevclub@users.noreply.github.com>
2022-08-18 09:34:02 +0100

    add comment at display

--- src/assets/js/galeria.js
+++ src/assets/js/galeria.js
@@ -18,16 +18,21 @@ closeGaleria.addEventListener('click',()=>{
 function inicial(){
 
     const stack_itens = document.querySelectorAll('.stack-itens img')
+    const descricao = document.querySelector('.galeria-descricao p')
+    
     display.setAttribute('src',stack_itens[0].getAttribute('src'))
+    descricao.textContent = stack_itens[0].getAttribute('alt')
 
     stack_itens.forEach(iten=>{
         iten.addEventListener('click',(event)=>{
             display.setAttribute('src',event.target.getAttribute('src'))
-            //console.log(event.target.getAttribute('src'))
+            descricao.textContent = event.target.getAttribute('alt')
+            console.log(event.target)
         })
     })
 
 }
+
 
 
 /*ATRIBUIR OS ITENS NA LI'S DA GALERIA*/
@@ -47,7 +52,7 @@ function handleSpan(data){
     data.forEach(span=>{
         stack_ul.innerHTML  += `
         <li>
-        <img src="${span.dataset.source}" alt="">
+        <img src="${span.dataset.source}" alt="${span.textContent}">
         </li>
         `
     })
